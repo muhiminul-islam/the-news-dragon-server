12ba43a2b0c87c68ac5b16df7bd60c901265ac24
muhiminul-islam <islammuhiminul11@gmail.com>
2023-04-30 02:11:34 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const categories = require('./data/categories.json');
+const news = require('./data/news.json');
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Dragon is running')
+});
+
+app.get('/categories', (req, res) => {
+    res.send(categories);
+})
+app.get('/news', (req, res) => {
+    res.send(news);
+})
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedItems = news.find(n => n._id === id)
+    res.send(selectedItems);
+
+})
+
+app.get('/categories/:id', (req, res) => {
+    const id = req.params.id;
+    if (id == 0) {
+        res.send(news);
+    }
+    else {
+        const categoryItem = news.filter(n => n.category_id === id);
+        res.send(categoryItem)
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Dragon API is running on port: ${port}`)
+})
